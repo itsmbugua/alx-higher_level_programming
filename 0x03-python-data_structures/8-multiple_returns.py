39276d8132059998d5d3fb3179594b9075b5b294
itsmbugua <samisaacmbugua@gmail.com>
2023-06-12 14:35:28 -0700

    correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
+# 8-multiple_returns.py
 
-def delete_at(my_list: list = [], idx: int = 0) -> list:
-    """ function that deletes an item at specific index
 
-        Args:
-            my_list: array to delete item from
-            idx: index to find item
-
-        Return:
-             list without item at index idx else,
-             return just my_list
-    """
-
-    length = len(my_list)
-
-    if idx < 0 or idx > length:
-        return my_list
-
-    del my_list[idx]
-
-    return my_list
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
