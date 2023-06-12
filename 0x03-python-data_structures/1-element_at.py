6ac9aaf46ff0ffe7dc4525f5f47f5d5fe787cc9c
itsmbugua <samisaacmbugua@gmail.com>
2023-06-12 14:39:25 -0700

    correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-
-def element_at(my_list: list, idx: int) -> int:
-    """ function to get element at specified index
-
-        Args:
-            my_list: an array of integers
-            idx: index to find item
-
-        Returns:
-            the integer at idx and if fails returns None
-
-    """
-    length = len(my_list) - 1
-
-    if idx < 0 or idx > length:
-        return None
-
-    return my_list[idx]    
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()    
