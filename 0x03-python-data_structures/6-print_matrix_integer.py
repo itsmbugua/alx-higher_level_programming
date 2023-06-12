f27236679ff6f034feb592b8e61dc9a9a7ecf81e
itsmbugua <samisaacmbugua@gmail.com>
2023-06-12 14:37:33 -0700

    correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
 
-def print_matrix_integer(matrix: list = [[]]) -> None:
-    """ print a matrix of integers
-
-    Args:
-    two dimensional array of integers
-
-    Return:
-    None
-
-    """
-    for x in matrix:
-        for j in range(len(x)):
-            if j == len(x) - 1:
-                print("{}".format(x[j]))
-            else:print("{}".format(x[j]), end=" ")
